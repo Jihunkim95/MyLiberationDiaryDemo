eccc0ed8f6a88b41ca0de2bef8bfcffec79f841a
Jihunkim95 <darak551@naver.com>
2023-12-28 19:54:18 +0900

    init: 초기 설정

---
+++ MyLiberationDiaryDemo/MyLiberationDiaryDemo/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  MyLiberationDiaryDemo
+//
+//  Created by 김지훈 on 2023/12/28.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
